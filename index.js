04274f08ed771f2abac49461c054c464a732c0e3
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-19 10:37:23 -0500

    commit tree 715c78902c34ada42ad6c63e86ccea6974bdc374

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+// function hasTargetSum(array, target) {
+//   for(let i = 0; i < array.length; i ++)  {
+//     const currentArray = array[i]
+//     const difference =  target - currentArray
+//   for(let j = i + 1; j < array.length; j ++) {
+//     const currentArrayJ = array[j]
+//     if(currentArrayJ === difference) {return true}
+//     }
+//     } 
+//     return false
+//   }
+  //Update based on walkthrough to optimize 
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (seenNumbers[complement]) return true;
+    seenNumbers[array[i]] = true;
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2), iterating over array twice 
 */
 
 /* 
   Add your pseudocode here
+  		Function called hasTargetSum takes two arguments an array of integers and a target integer
+      create consts to hold new arrays 
+      iterate twice over array
+			If…else statement for truthy and falsey results 
+
 */
 
 /*
@@ -29,6 +54,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  //my tests
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 8, 9, 43, 86], 52));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([32, 54, 64, 77, 90], 45));
 }
 
 module.exports = hasTargetSum;
